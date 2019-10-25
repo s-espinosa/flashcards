22a4fd7aff7c2e7c1bdf440d1821fff147aed351
Sal Espinosa <sespinos@gmail.com>
2019-10-24 18:01:21 -0600

    Finish round

--- test/round_test.js
+++ test/round_test.js
@@ -55,9 +55,80 @@ describe('Round', () => {
       let round = new Round(deck)
 
       assert.equal(round.currentCard(), card1)
+
       round.takeTurn("Answer")
       assert.instanceOf(round.turns[0], Turn)
     })
   })
 
+  describe('#numberCorrect', () => {
+    it('calculates the number of correct Turns', () => {
+      let card1 = new Card("Question?", "Answer", "Test Question")
+      let card2 = new Card("Question 2?", "Answer 2", "Test Question")
+      let card3 = new Card("Question 3?", "Answer 3", "Pop Culture")
+      let cards = [card1, card2, card3]
+      let deck = new Deck(cards)
+      let round = new Round(deck)
+
+      round.takeTurn("Answer")
+
+      assert.equal(round.numberCorrect(), 1)
+    })
+  })
+
+  describe('#numberCorrectByCategory', () => {
+    it('calculates the number of correct Turns in a category', () => {
+      let card1 = new Card("Question?", "Answer", "Test Question")
+      let card2 = new Card("Question 2?", "Answer 2", "Test Question")
+      let card3 = new Card("Question 3?", "Answer 3", "Pop Culture")
+      let cards = [card1, card2, card3]
+      let deck = new Deck(cards)
+      let round = new Round(deck)
+
+      round.takeTurn("Answer")
+      assert.equal(round.numberCorrectByCategory("Test Question"), 1)
+      round.takeTurn("Another Answer.")
+      assert.equal(round.numberCorrectByCategory("Test Question"), 1)
+      round.takeTurn("Answer 3")
+      assert.equal(round.numberCorrectByCategory("Test Question"), 1)
+    })
+  })
+
+  describe('#percentCorrect', () => {
+    it('calculates the number of correct Turns', () => {
+      let card1 = new Card("Question?", "Answer", "Test Question")
+      let card2 = new Card("Question 2?", "Answer 2", "Test Question")
+      let card3 = new Card("Question 3?", "Answer 3", "Pop Culture")
+      let cards = [card1, card2, card3]
+      let deck = new Deck(cards)
+      let round = new Round(deck)
+
+      round.takeTurn("Answer")
+      assert.equal(round.percentCorrect(), 100.0)
+
+      round.takeTurn("Answer")
+      assert.equal(round.percentCorrect(), 50.0)
+    })
+  })
+
+  describe('#percentCorrectByCategory', () => {
+    it('calculates the number of correct Turns in a category', () => {
+      let card1 = new Card("Question?", "Answer", "Test Question")
+      let card2 = new Card("Question 2?", "Answer 2", "Test Question")
+      let card3 = new Card("Question 3?", "Answer 3", "Pop Culture")
+      let cards = [card1, card2, card3]
+      let deck = new Deck(cards)
+      let round = new Round(deck)
+
+      round.takeTurn("Answer")
+      assert.equal(round.percentCorrectByCategory("Test Question"), 100.0)
+
+      round.takeTurn("Answer")
+      assert.equal(round.percentCorrectByCategory("Test Question"), 50.0)
+
+      round.takeTurn("Answer 3")
+      assert.equal(round.percentCorrectByCategory("Test Question"), 50.0)
+      assert.equal(round.percentCorrectByCategory("Pop Culture"), 100.0)
+    })
+  })
 })
