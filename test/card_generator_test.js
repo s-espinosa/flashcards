1c45ca13c3a60f2013ab08fe6fbac6b3dbe50467
Sal Espinosa <sespinos@gmail.com>
2019-10-28 17:30:38 -0600

    Add CardGenerator and tests

---
+++ test/card_generator_test.js
@@ -0,0 +1,15 @@
+let assert = require('chai').assert
+let CardGenerator = require('../lib/card_generator')
+let Card = require('../lib/card')
+
+describe('CardGenerator', () => {
+  describe('::new', () => {
+    it('should create an array of Card objects', () => {
+      let filepath = '../data/cards.csv'
+      let cg = new CardGenerator(filepath)
+
+      assert.instanceOf(cg.cards, Array)
+      assert.instanceOf(cg.cards[0], Card)
+    })
+  })
+})
