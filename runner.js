de8fd750fc0ee12ecbb05588688d4c2c90a9ee76
Sal Espinosa <sespinos@gmail.com>
2019-10-28 17:59:36 -0600

    Add feedback to runner

--- runner.js
+++ runner.js
@@ -17,7 +17,8 @@ console.log(prompts.intro())
 for(let i = 0; i < cards.length; i++) {
   console.log(prompts.currentCard())
   let info = input.question((round.currentQuestion() + "\n"))
-  round.takeTurn(info)
+  let turn = round.takeTurn(info)
+  console.log(turn.feedback())
 }
 
 console.log(prompts.feedback(round))
